7bad921b011a2c50fcf15f7f0f861a8c2c141fcf
Sohailul Islam Alvi <94545356+alvi-codes@users.noreply.github.com>
2023-07-14 17:29:08 +0600

    Update Egg_Quality_Checker.cpp

--- C++/Egg_Quality_Checker.cpp
+++ C++/Egg_Quality_Checker.cpp
@@ -22,18 +22,19 @@ int main()
         else if(curr_weight<50){
             faulty++;
         }
-        else{
+        else if (curr_weight>70){
             non_ideal++;
-        }
-    }
     
+   
+        }
+    } 
     if(faulty>4){
         std::cout<<"The box contains more than four faulty eggs"<<std::endl;
     }
     else{
-        std::cout<<"Count of eggs with ideal weight ="<<ideal<<std::endl;
-        std::cout<<"Count of eggs with non-ideal weight ="<<non_ideal<<std::endl;
-        std::cout<<"Count of eggs with faulty weight ="<<faulty<<std::endl;
+        std::cout<<"Count of eggs with ideal weight="<<ideal<<std::endl;
+        std::cout<<"Count of eggs with non-ideal weight="<<non_ideal<<std::endl;
+        std::cout<<"Count of eggs with faulty weight="<<faulty<<std::endl;
     }
     
     return 0;
